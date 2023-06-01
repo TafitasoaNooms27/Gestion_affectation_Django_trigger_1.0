02f7b50dd26e1f5b1483792450e1e4e064659d94
TafitasoaNooms27 <rrnomenatafitasoa@gmail.com>
2023-06-01 22:34:13 +0300

    tena farany ay

--- gestion_affectation2/gestion_affectation2/settings.py
+++ gestion_affectation2/gestion_affectation2/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "gestion_affectation2.urls"
+ROOT_URLCONF = "gestion_affectation.urls"
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "gestion_affectation2.wsgi.application"
+WSGI_APPLICATION = "gestion_affectation.wsgi.application"
 
 
 # Database
