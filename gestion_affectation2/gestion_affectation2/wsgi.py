3d73a185b295b4a3d971bf1921239e43d3270de2
TafitasoaNooms27 <rrnomenatafitasoa@gmail.com>
2023-06-02 08:36:51 +0300

    nomination affectetion2 lasa affectation fotsiny

--- gestion_affectation2/gestion_affectation2/wsgi.py
+++ gestion_affectation2/gestion_affectation2/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for gestion_affectation2 project.
+WSGI config for gestion_affectation project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gestion_affectation2.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gestion_affectation.settings")
 
 application = get_wsgi_application()
